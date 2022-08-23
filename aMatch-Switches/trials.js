9b1688846696354cde61967d422e74931b6216fd
JasonKChow <jason.k.chow@vanderbilt.edu>
2022-08-23 13:27:07 -0500

    add auditory tests

---
+++ aMatch-Switches/trials.js
@@ -0,0 +1,366 @@
+// Test trials
+let trials = [
+    {
+        "trialN": 1,
+        "Stim1": "CherryBrown2.mp3",
+        "Stim2": "CherryBrown3Noise.mp3",
+        "CorrRes": "f"
+    },
+    {
+        "trialN": 2,
+        "Stim1": "Aliaz100g1Noise.mp3",
+        "Stim2": "Aliaz100g2.mp3",
+        "CorrRes": "f"
+    },
+    {
+        "trialN": 3,
+        "Stim1": "Aliaz100g1.mp3",
+        "Stim2": "CherryBrown3Noise.mp3",
+        "CorrRes": "j"
+    },
+    {
+        "trialN": 4,
+        "Stim1": "Aliaz100g4.mp3",
+        "Stim2": "Aliaz100g5.mp3",
+        "CorrRes": "f"
+    },
+    {
+        "trialN": 5,
+        "Stim1": "Aliaz100g1.mp3",
+        "Stim2": "CherryRed4Noise.mp3",
+        "CorrRes": "j"
+    },
+    {
+        "trialN": 6,
+        "Stim1": "Aliaz100g3.mp3",
+        "Stim2": "Aliaz100g2.mp3",
+        "CorrRes": "f"
+    },
+    {
+        "trialN": 7,
+        "Stim1": "CherryGreen6Noise.mp3",
+        "Stim2": "Aliaz100g6Noise.mp3",
+        "CorrRes": "j"
+    },
+    {
+        "trialN": 8,
+        "Stim1": "CherryRed4.mp3",
+        "Stim2": "CherryGreen4Noise.mp3",
+        "CorrRes": "j"
+    },
+    {
+        "trialN": 9,
+        "Stim1": "CherryBrown2.mp3",
+        "Stim2": "CherryGreen6.mp3",
+        "CorrRes": "j"
+    },
+    {
+        "trialN": 10,
+        "Stim1": "CherryGreen5.mp3",
+        "Stim2": "CherryGreen4.mp3",
+        "CorrRes": "f"
+    },
+    {
+        "trialN": 11,
+        "Stim1": "CherryGreen1.mp3",
+        "Stim2": "CherryGreen4.mp3",
+        "CorrRes": "f"
+    },
+    {
+        "trialN": 12,
+        "Stim1": "CherryGreen1.mp3",
+        "Stim2": "CherryGreen3.mp3",
+        "CorrRes": "f"
+    },
+    {
+        "trialN": 13,
+        "Stim1": "CherryBrown2Noise.mp3",
+        "Stim2": "CherryGreen6Noise.mp3",
+        "CorrRes": "j"
+    },
+    {
+        "trialN": 14,
+        "Stim1": "KailhSpeedBronze1.mp3",
+        "Stim2": "KailhSpeedBronze5.mp3",
+        "CorrRes": "f"
+    },
+    {
+        "trialN": 15,
+        "Stim1": "KailhSpeedBronze6.mp3",
+        "Stim2": "KailhSpeedBronze4.mp3",
+        "CorrRes": "f"
+    },
+    {
+        "trialN": 16,
+        "Stim1": "CherryBrown1Noise.mp3",
+        "Stim2": "CherryBrown2.mp3",
+        "CorrRes": "f"
+    },
+    {
+        "trialN": 17,
+        "Stim1": "KailhSpeedBronze4Noise.mp3",
+        "Stim2": "CherryRed5.mp3",
+        "CorrRes": "j"
+    },
+    {
+        "trialN": 18,
+        "Stim1": "GateronSilentBrown4Noise.mp3",
+        "Stim2": "CherryGreen4.mp3",
+        "CorrRes": "j"
+    },
+    {
+        "trialN": 19,
+        "Stim1": "CherryGreen1.mp3",
+        "Stim2": "CherryGreen4Noise.mp3",
+        "CorrRes": "f"
+    },
+    {
+        "trialN": 20,
+        "Stim1": "CherryRed5.mp3",
+        "Stim2": "CherryRed6.mp3",
+        "CorrRes": "f"
+    },
+    {
+        "trialN": 21,
+        "Stim1": "Aliaz100g4Noise.mp3",
+        "Stim2": "KailhSpeedBronze4Noise.mp3",
+        "CorrRes": "j"
+    },
+    {
+        "trialN": 22,
+        "Stim1": "GateronSilentBrown6.mp3",
+        "Stim2": "KailhSpeedBronze6Noise.mp3",
+        "CorrRes": "j"
+    },
+    {
+        "trialN": 23,
+        "Stim1": "Aliaz100g6.mp3",
+        "Stim2": "Aliaz100g3.mp3",
+        "CorrRes": "f"
+    },
+    {
+        "trialN": 24,
+        "Stim1": "CherryRed6.mp3",
+        "Stim2": "CherryRed4Noise.mp3",
+        "CorrRes": "f"
+    },
+    {
+        "trialN": 25,
+        "Stim1": "GateronSilentBrown3Noise.mp3",
+        "Stim2": "CherryBrown5.mp3",
+        "CorrRes": "j"
+    },
+    {
+        "trialN": 26,
+        "Stim1": "Aliaz100g5Noise.mp3",
+        "Stim2": "Aliaz100g6Noise.mp3",
+        "CorrRes": "f"
+    },
+    {
+        "trialN": 27,
+        "Stim1": "CherryGreen4.mp3",
+        "Stim2": "CherryGreen6.mp3",
+        "CorrRes": "f"
+    },
+    {
+        "trialN": 28,
+        "Stim1": "KailhSpeedBronze5Noise.mp3",
+        "Stim2": "CherryBrown5.mp3",
+        "CorrRes": "j"
+    },
+    {
+        "trialN": 29,
+        "Stim1": "KailhSpeedBronze6.mp3",
+        "Stim2": "KailhSpeedBronze5.mp3",
+        "CorrRes": "f"
+    },
+    {
+        "trialN": 30,
+        "Stim1": "GateronSilentBrown4.mp3",
+        "Stim2": "GateronSilentBrown6.mp3",
+        "CorrRes": "f"
+    }
+]
+
+let trials2 = [
+    {
+        "trialN": 31,
+        "Stim1": "CherryGreen6.mp3",
+        "Stim2": "Aliaz100g6Noise.mp3",
+        "CorrRes": "j"
+    },
+    {
+        "trialN": 32,
+        "Stim1": "GateronSilentBrown6.mp3",
+        "Stim2": "CherryGreen6.mp3",
+        "CorrRes": "j"
+    },
+    {
+        "trialN": 33,
+        "Stim1": "KailhSpeedBronze3.mp3",
+        "Stim2": "KailhSpeedBronze6.mp3",
+        "CorrRes": "f"
+    },
+    {
+        "trialN": 34,
+        "Stim1": "CherryRed4Noise.mp3",
+        "Stim2": "CherryBrown5.mp3",
+        "CorrRes": "j"
+    },
+    {
+        "trialN": 35,
+        "Stim1": "Aliaz100g1.mp3",
+        "Stim2": "Aliaz100g6.mp3",
+        "CorrRes": "f"
+    },
+    {
+        "trialN": 36,
+        "Stim1": "CherryRed3.mp3",
+        "Stim2": "CherryRed1.mp3",
+        "CorrRes": "f"
+    },
+    {
+        "trialN": 37,
+        "Stim1": "CherryRed2.mp3",
+        "Stim2": "CherryRed4.mp3",
+        "CorrRes": "f"
+    },
+    {
+        "trialN": 38,
+        "Stim1": "CherryBrown5.mp3",
+        "Stim2": "CherryBrown6.mp3",
+        "CorrRes": "f"
+    },
+    {
+        "trialN": 39,
+        "Stim1": "CherryGreen5Noise.mp3",
+        "Stim2": "CherryRed5Noise.mp3",
+        "CorrRes": "j"
+    },
+    {
+        "trialN": 40,
+        "Stim1": "Aliaz100g5.mp3",
+        "Stim2": "KailhSpeedBronze6Noise.mp3",
+        "CorrRes": "j"
+    },
+    {
+        "trialN": 41,
+        "Stim1": "CherryBrown6.mp3",
+        "Stim2": "CherryBrown4.mp3",
+        "CorrRes": "f"
+    },
+    {
+        "trialN": 42,
+        "Stim1": "Aliaz100g6.mp3",
+        "Stim2": "CherryRed6Noise.mp3",
+        "CorrRes": "j"
+    },
+    {
+        "trialN": 43,
+        "Stim1": "CherryGreen4.mp3",
+        "Stim2": "CherryGreen2.mp3",
+        "CorrRes": "f"
+    },
+    {
+        "trialN": 44,
+        "Stim1": "CherryBrown1.mp3",
+        "Stim2": "GateronSilentBrown1.mp3",
+        "CorrRes": "j"
+    },
+    {
+        "trialN": 45,
+        "Stim1": "CherryGreen6.mp3",
+        "Stim2": "KailhSpeedBronze1Noise.mp3",
+        "CorrRes": "j"
+    },
+    {
+        "trialN": 46,
+        "Stim1": "GateronSilentBrown5.mp3",
+        "Stim2": "Aliaz100g5.mp3",
+        "CorrRes": "j"
+    },
+    {
+        "trialN": 47,
+        "Stim1": "GateronSilentBrown1Noise.mp3",
+        "Stim2": "GateronSilentBrown3.mp3",
+        "CorrRes": "f"
+    },
+    {
+        "trialN": 48,
+        "Stim1": "CherryGreen3.mp3",
+        "Stim2": "KailhSpeedBronze6Noise.mp3",
+        "CorrRes": "j"
+    },
+    {
+        "trialN": 49,
+        "Stim1": "CherryRed6Noise.mp3",
+        "Stim2": "GateronSilentBrown1.mp3",
+        "CorrRes": "j"
+    },
+    {
+        "trialN": 50,
+        "Stim1": "CherryBrown6Noise.mp3",
+        "Stim2": "GateronSilentBrown1.mp3",
+        "CorrRes": "j"
+    },
+    {
+        "trialN": 51,
+        "Stim1": "CherryBrown3.mp3",
+        "Stim2": "CherryBrown5.mp3",
+        "CorrRes": "f"
+    },
+    {
+        "trialN": 52,
+        "Stim1": "Aliaz100g6.mp3",
+        "Stim2": "CherryBrown6.mp3",
+        "CorrRes": "j"
+    },
+    {
+        "trialN": 53,
+        "Stim1": "CherryRed6.mp3",
+        "Stim2": "CherryRed4.mp3",
+        "CorrRes": "f"
+    },
+    {
+        "trialN": 54,
+        "Stim1": "KailhSpeedBronze6.mp3",
+        "Stim2": "CherryBrown6Noise.mp3",
+        "CorrRes": "j"
+    },
+    {
+        "trialN": 55,
+        "Stim1": "CherryGreen6Noise.mp3",
+        "Stim2": "CherryRed6Noise.mp3",
+        "CorrRes": "j"
+    },
+    {
+        "trialN": 56,
+        "Stim1": "GateronSilentBrown4.mp3",
+        "Stim2": "GateronSilentBrown3.mp3",
+        "CorrRes": "f"
+    },
+    {
+        "trialN": 57,
+        "Stim1": "GateronSilentBrown6.mp3",
+        "Stim2": "Aliaz100g6.mp3",
+        "CorrRes": "j"
+    },
+    {
+        "trialN": 58,
+        "Stim1": "CherryRed3.mp3",
+        "Stim2": "CherryBrown1.mp3",
+        "CorrRes": "j"
+    },
+    {
+        "trialN": 59,
+        "Stim1": "CherryGreen6.mp3",
+        "Stim2": "CherryGreen4.mp3",
+        "CorrRes": "f"
+    },
+    {
+        "trialN": 60,
+        "Stim1": "KailhSpeedBronze3Noise.mp3",
+        "Stim2": "CherryRed2Noise.mp3",
+        "CorrRes": "j"
+    }
+]
---
+++ aNOMT-Birds/trials.js
@@ -0,0 +1,338 @@
+let trials = [
+    {
+        "TrialNum": 1,
+        "Target": "Acadian Flycatcher 1.mp3",
+        "Foil1": "Blackthroated Blue Warbler 1.mp3",
+        "Foil2": "Brown Thrasher 1.mp3",
+        "TargetLoc": 3
+    },
+    {
+        "TrialNum": 2,
+        "Target": "Acadian Flycatcher 2.mp3",
+        "Foil1": "Carolina Wren 1.mp3",
+        "Foil2": "Eastern Towhee 1.mp3",
+        "TargetLoc": 2
+    },
+    {
+        "TrialNum": 3,
+        "Target": "Acadian Flycatcher 3.mp3",
+        "Foil1": "Eurasian Blackbird 1.mp3",
+        "Foil2": "Hermit Thrush 1.mp3",
+        "TargetLoc": 2
+    },
+    {
+        "TrialNum": 4,
+        "Target": "American Redstart1.mp3",
+        "Foil1": "Wood Thrush 2.mp3",
+        "Foil2": "Winter Wren 2.mp3",
+        "TargetLoc": 3
+    },
+    {
+        "TrialNum": 5,
+        "Target": "American Redstart2.mp3",
+        "Foil1": "Whitecrowned Sparrow 2.mp3",
+        "Foil2": "Western Meadowlark2.mp3",
+        "TargetLoc": 3
+    },
+    {
+        "TrialNum": 6,
+        "Target": "American Redstart3.mp3",
+        "Foil1": "Vesper Sparrow 2.mp3",
+        "Foil2": "Veery2.mp3",
+        "TargetLoc": 3
+    },
+    {
+        "TrialNum": 7,
+        "Target": "American Robin1.mp3",
+        "Foil1": "Hooded Warbler3.mp3",
+        "Foil2": "Sedge Wren7.mp3",
+        "TargetLoc": 2
+    },
+    {
+        "TrialNum": 8,
+        "Target": "American Robin2.mp3",
+        "Foil1": "Brown Thrasher 3.mp3",
+        "Foil2": "Eurasian Blackbird3.mp3",
+        "TargetLoc": 1
+    },
+    {
+        "TrialNum": 9,
+        "Target": "American Robin3.mp3",
+        "Foil1": "Connecticut Warbler 3.mp3",
+        "Foil2": "Gray Catbird 3.mp3",
+        "TargetLoc": 1
+    },
+    {
+        "TrialNum": 10,
+        "Target": "Blackpoll Warbler1.mp3",
+        "Foil1": "Wood Thrush 4.mp3",
+        "Foil2": "Whitecrowned Sparrow4.mp3",
+        "TargetLoc": 1
+    },
+    {
+        "TrialNum": 11,
+        "Target": "Blackpoll Warbler 11.mp3",
+        "Foil1": "Cerulean Warbler 11.mp3",
+        "Foil2": "Eurasian Blackbird 11.mp3",
+        "TargetLoc": 3
+    },
+    {
+        "TrialNum": 12,
+        "Target": "Blackpoll Warbler3.mp3",
+        "Foil1": "Veery4.mp3",
+        "Foil2": "Swainsons Thrush 4.mp3",
+        "TargetLoc": 1
+    },
+    {
+        "TrialNum": 13,
+        "Target": "Blackthroated Green Warbler1.mp3",
+        "Foil1": "Northern Cardinal 5.mp3",
+        "Foil2": "Sedge Wren 5.mp3",
+        "TargetLoc": 1
+    },
+    {
+        "TrialNum": 14,
+        "Target": "Blackthroated Green Warbler 2.mp3",
+        "Foil1": "Northern Mockingbird 5.mp3",
+        "Foil2": "House Finch 5.mp3",
+        "TargetLoc": 2
+    },
+    {
+        "TrialNum": 15,
+        "Target": "Blackthroated Green Warbler 3.mp3",
+        "Foil1": "Hermit Thrush 5.mp3",
+        "Foil2": "Gray Catbird 5.mp3",
+        "TargetLoc": 3
+    },
+    {
+        "TrialNum": 16,
+        "Target": "Chipping Sparrow1.mp3",
+        "Foil1": "Western Meadowlark 6.mp3",
+        "Foil2": "Hooded Warbler 6.mp3",
+        "TargetLoc": 2
+    },
+    {
+        "TrialNum": 17,
+        "Target": "Chipping Sparrow2.mp3",
+        "Foil1": "Swainsons Thrush 6.mp3",
+        "Foil2": "Hermit Thrush 6.mp3",
+        "TargetLoc": 3
+    },
+    {
+        "TrialNum": 18,
+        "Target": "Chipping Sparrow3.mp3",
+        "Foil1": "Gray Catbird 18.mp3",
+        "Foil2": "Housefinch 18.mp3",
+        "TargetLoc": 3
+    },
+    {
+        "TrialNum": 19,
+        "Target": "American Robin4.mp3",
+        "Foil1": "Graycheeked Thrush3.mp3",
+        "Foil2": "Hermit Thrush 3.mp3",
+        "TargetLoc": 3
+    },
+    {
+        "TrialNum": 20,
+        "Target": "Blackpoll Warbler4.mp3",
+        "Foil1": "Sedge Wren4.mp3",
+        "Foil2": "Rubycrowned Kinglet4.mp3",
+        "TargetLoc": 1
+    },
+    {
+        "TrialNum": 21,
+        "Target": "American Robin5.mp3",
+        "Foil1": "Western Meadowlark3.mp3",
+        "Foil2": "Vesper Sparrow3.mp3",
+        "TargetLoc": 2
+    },
+    {
+        "TrialNum": 22,
+        "Target": "Chipping Sparrow4.mp3",
+        "Foil1": "Veery6.mp3",
+        "Foil2": "Northern Cardinal6.mp3",
+        "TargetLoc": 1
+    },
+    {
+        "TrialNum": 23,
+        "Target": "American Robin7.mp3",
+        "Foil1": "Rubycrowned Kinglet3.mp3",
+        "Foil2": "Swainsons Thrush3.mp3",
+        "TargetLoc": 3
+    },
+    {
+        "TrialNum": 24,
+        "Target": "Acadian Flycatcher 7.mp3",
+        "Foil1": "House Finch1.mp3",
+        "Foil2": "Rubycrowned Kinglet1.mp3",
+        "TargetLoc": 2
+    },
+    {
+        "TrialNum": 25,
+        "Target": "Acadian Flycatcher 4.mp3",
+        "Foil1": "Hooded Warbler1.mp3",
+        "Foil2": "Northern Cardinal1.mp3",
+        "TargetLoc": 3
+    },
+    {
+        "TrialNum": 26,
+        "Target": "Blackthroated Green Warbler5.mp3",
+        "Foil1": "Lincolns Sparrow5.mp3",
+        "Foil2": "Rubycrowned Kinglet5.mp3",
+        "TargetLoc": 3
+    },
+    {
+        "TrialNum": 27,
+        "Target": "American Robin6.mp3",
+        "Foil1": "Carolina Wren3.mp3",
+        "Foil2": "Northern Mockingbird3.mp3",
+        "TargetLoc": 3
+    },
+    {
+        "TrialNum": 28,
+        "Target": "Acadian Flycatcher 8.mp3",
+        "Foil1": "Swainsons Thrush1.mp3",
+        "Foil2": "Yellow Warbler Northern1.mp3",
+        "TargetLoc": 1
+    },
+    {
+        "TrialNum": 29,
+        "Target": "Blackthroated Green Warbler4.mp3",
+        "Foil1": "Graycheeked Thrush5.mp3",
+        "Foil2": "Eurasian Blackbird5.mp3",
+        "TargetLoc": 3
+    },
+    {
+        "TrialNum": 30,
+        "Target": "American Redstart 4.mp3",
+        "Foil1": "Swainsons Thrush2.mp3",
+        "Foil2": "House Finch2.mp3",
+        "TargetLoc": 2
+    },
+    {
+        "TrialNum": 31,
+        "Target": "Blackthroated Green Warbler6.mp3",
+        "Foil1": "Ovenbird5.mp3",
+        "Foil2": "Hooded Warbler5.mp3",
+        "TargetLoc": 3
+    },
+    {
+        "TrialNum": 32,
+        "Target": "Blackpoll Warbler6.mp3",
+        "Foil1": "Yellow Warbler Northern4.mp3",
+        "Foil2": "Northern Cardinal4.mp3",
+        "TargetLoc": 3
+    },
+    {
+        "TrialNum": 33,
+        "Target": "Acadian Flycatcher 6.mp3",
+        "Foil1": "Connecticut Warbler1.mp3",
+        "Foil2": "Graycheeked Thrush1.mp3",
+        "TargetLoc": 3
+    },
+    {
+        "TrialNum": 34,
+        "Target": "Chipping Sparrow8.mp3",
+        "Foil1": "Winter Wren6.mp3",
+        "Foil2": "Lincolns Sparrow6.mp3",
+        "TargetLoc": 2
+    },
+    {
+        "TrialNum": 35,
+        "Target": "Blackpoll Warbler8.mp3",
+        "Foil1": "Yellowthroated Warbler4.mp3",
+        "Foil2": "Hooded Warbler4.mp3",
+        "TargetLoc": 3
+    },
+    {
+        "TrialNum": 36,
+        "Target": "American Redstart5.mp3",
+        "Foil1": "Lincolns Sparrow2.mp3",
+        "Foil2": "Hooded Warbler2.mp3",
+        "TargetLoc": 2
+    },
+    {
+        "TrialNum": 37,
+        "Target": "Blackpoll Warbler5.mp3",
+        "Foil1": "Tennessee Warbler4.mp3",
+        "Foil2": "Vesper Sparrow4.mp3",
+        "TargetLoc": 3
+    },
+    {
+        "TrialNum": 38,
+        "Target": "Chipping Sparrow5.mp3",
+        "Foil1": "Ovenbird6.mp3",
+        "Foil2": "Northern Mockingbird6.mp3",
+        "TargetLoc": 3
+    },
+    {
+        "TrialNum": 39,
+        "Target": "Acadian Flycatcher 5.mp3",
+        "Foil1": "Veery 39.mp3",
+        "Foil2": "Lincolns Sparrow1.mp3",
+        "TargetLoc": 3
+    },
+    {
+        "TrialNum": 40,
+        "Target": "American Redstart7.mp3",
+        "Foil1": "Connecticut Warbler2.mp3",
+        "Foil2": "Tennessee Warbler2.mp3",
+        "TargetLoc": 3
+    },
+    {
+        "TrialNum": 41,
+        "Target": "Blackthroated Green Warbler7.mp3",
+        "Foil1": "Connecticut Warbler5.mp3",
+        "Foil2": "Blackthroated Blue Warbler5.mp3",
+        "TargetLoc": 3
+    },
+    {
+        "TrialNum": 42,
+        "Target": "Blackpoll Warbler 42.mp3",
+        "Foil1": "Yellowrumped Warbler Myrtle4.mp3",
+        "Foil2": "Cerulean Warbler 42.mp3",
+        "TargetLoc": 1
+    },
+    {
+        "TrialNum": 43,
+        "Target": "Chipping Sparrow6.mp3",
+        "Foil1": "Tennessee Warbler.mp3",
+        "Foil2": "Rubycrowned Kinglet6.mp3",
+        "TargetLoc": 2
+    },
+    {
+        "TrialNum": 44,
+        "Target": "Blackthroated Green Warbler8.mp3",
+        "Foil1": "Yellow Warbler Northern5.mp3",
+        "Foil2": "Yellowrumped Warbler Myrtle5.mp3",
+        "TargetLoc": 3
+    },
+    {
+        "TrialNum": 45,
+        "Target": "American Redstart6.mp3",
+        "Foil1": "Yellowthroated Warbler2.mp3",
+        "Foil2": "Blackthroated Blue Warbler2.mp3",
+        "TargetLoc": 2
+    },
+    {
+        "TrialNum": 46,
+        "Target": "Chipping Sparrow7.mp3",
+        "Foil1": "Whitecrowned Sparrow6.mp3",
+        "Foil2": "Vesper Sparrow6.mp3",
+        "TargetLoc": 1
+    },
+    {
+        "TrialNum": 47,
+        "Target": "American Robin8.mp3",
+        "Foil1": "Northern Cardinal3.mp3",
+        "Foil2": "Ovenbird3.mp3",
+        "TargetLoc": 3
+    },
+    {
+        "TrialNum": 48,
+        "Target": "American Redstart 48.mp3",
+        "Foil1": "Yellowrumped Warbler Myrtle2.mp3",
+        "Foil2": "Northern Mockingbird 48.mp3",
+        "TargetLoc": 3
+    }
+];
