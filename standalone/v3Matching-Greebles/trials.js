d5d15b5f3b31959969c3cceb14fa8f7277e20fb9
JasonKChow <jason.k.chow@vanderbilt.edu>
2022-09-06 18:06:05 -0500

    standalone versions of psychOmetrics tests

---
+++ standalone/v3Matching-Greebles/trials.js
@@ -0,0 +1,308 @@
+const trials = [
+    {
+        "Trial": 1,
+        "CorrRes": 1,
+        "Duration": 1000,
+        "Feedback": true
+    },
+    {
+        "Trial": 2,
+        "CorrRes": 1,
+        "Duration": 1000,
+        "Feedback": true
+    },
+    {
+        "Trial": 3,
+        "CorrRes": 3,
+        "Duration": 1000,
+        "Feedback": true
+    },
+    {
+        "Trial": 4,
+        "CorrRes": 1,
+        "Duration": 300,
+        "Feedback": false
+    },
+    {
+        "Trial": 5,
+        "CorrRes": 3,
+        "Duration": 300,
+        "Feedback": false
+    },
+    {
+        "Trial": 6,
+        "CorrRes": 3,
+        "Duration": 300,
+        "Feedback": false
+    },
+    {
+        "Trial": 7,
+        "CorrRes": 1,
+        "Duration": 300,
+        "Feedback": false
+    },
+    {
+        "Trial": 8,
+        "CorrRes": 3,
+        "Duration": 300,
+        "Feedback": false
+    },
+    {
+        "Trial": 9,
+        "CorrRes": 2,
+        "Duration": 300,
+        "Feedback": false
+    },
+    {
+        "Trial": 10,
+        "CorrRes": 2,
+        "Duration": 300,
+        "Feedback": false
+    },
+    {
+        "Trial": 11,
+        "CorrRes": 2,
+        "Duration": 300,
+        "Feedback": false
+    },
+    {
+        "Trial": 12,
+        "CorrRes": 1,
+        "Duration": 500,
+        "Feedback": false
+    },
+    {
+        "Trial": 13,
+        "CorrRes": 3,
+        "Duration": 500,
+        "Feedback": false
+    },
+    {
+        "Trial": 14,
+        "CorrRes": 2,
+        "Duration": 500,
+        "Feedback": false
+    },
+    {
+        "Trial": 15,
+        "CorrRes": 1,
+        "Duration": 500,
+        "Feedback": false
+    },
+    {
+        "Trial": 16,
+        "CorrRes": 2,
+        "Duration": 500,
+        "Feedback": false
+    },
+    {
+        "Trial": 17,
+        "CorrRes": 1,
+        "Duration": 500,
+        "Feedback": false
+    },
+    {
+        "Trial": 18,
+        "CorrRes": 3,
+        "Duration": 500,
+        "Feedback": false
+    },
+    {
+        "Trial": 19,
+        "CorrRes": 1,
+        "Duration": 500,
+        "Feedback": false
+    },
+    {
+        "Trial": 20,
+        "CorrRes": 1,
+        "Duration": 500,
+        "Feedback": false
+    },
+    {
+        "Trial": 21,
+        "CorrRes": 2,
+        "Duration": 500,
+        "Feedback": false
+    },
+    {
+        "Trial": 22,
+        "CorrRes": 1,
+        "Duration": 500,
+        "Feedback": false
+    },
+    {
+        "Trial": 23,
+        "CorrRes": 2,
+        "Duration": 500,
+        "Feedback": false
+    },
+    {
+        "Trial": 24,
+        "CorrRes": 3,
+        "Duration": 500,
+        "Feedback": false
+    },
+    {
+        "Trial": 25,
+        "CorrRes": 2,
+        "Duration": 500,
+        "Feedback": false
+    },
+    {
+        "Trial": 26,
+        "CorrRes": 3,
+        "Duration": 500,
+        "Feedback": false
+    },
+    {
+        "Trial": 27,
+        "CorrRes": 2,
+        "Duration": 500,
+        "Feedback": false
+    },
+    {
+        "Trial": 28,
+        "CorrRes": 1,
+        "Duration": 750,
+        "Feedback": false
+    },
+    {
+        "Trial": 29,
+        "CorrRes": 3,
+        "Duration": 750,
+        "Feedback": false
+    },
+    {
+        "Trial": 30,
+        "CorrRes": 2,
+        "Duration": 750,
+        "Feedback": false
+    },
+    {
+        "Trial": 31,
+        "CorrRes": 3,
+        "Duration": 750,
+        "Feedback": false
+    },
+    {
+        "Trial": 32,
+        "CorrRes": 2,
+        "Duration": 750,
+        "Feedback": false
+    },
+    {
+        "Trial": 33,
+        "CorrRes": 1,
+        "Duration": 750,
+        "Feedback": false
+    },
+    {
+        "Trial": 34,
+        "CorrRes": 3,
+        "Duration": 750,
+        "Feedback": false
+    },
+    {
+        "Trial": 35,
+        "CorrRes": 1,
+        "Duration": 750,
+        "Feedback": false
+    },
+    {
+        "Trial": 36,
+        "CorrRes": 2,
+        "Duration": 750,
+        "Feedback": false
+    },
+    {
+        "Trial": 37,
+        "CorrRes": 3,
+        "Duration": 750,
+        "Feedback": false
+    },
+    {
+        "Trial": 38,
+        "CorrRes": 1,
+        "Duration": 750,
+        "Feedback": false
+    },
+    {
+        "Trial": 39,
+        "CorrRes": 2,
+        "Duration": 750,
+        "Feedback": false
+    },
+    {
+        "Trial": 40,
+        "CorrRes": 3,
+        "Duration": 1000,
+        "Feedback": false
+    },
+    {
+        "Trial": 41,
+        "CorrRes": 3,
+        "Duration": 1000,
+        "Feedback": false
+    },
+    {
+        "Trial": 42,
+        "CorrRes": 1,
+        "Duration": 1000,
+        "Feedback": false
+    },
+    {
+        "Trial": 43,
+        "CorrRes": 1,
+        "Duration": 1000,
+        "Feedback": false
+    },
+    {
+        "Trial": 44,
+        "CorrRes": 3,
+        "Duration": 1000,
+        "Feedback": false
+    },
+    {
+        "Trial": 45,
+        "CorrRes": 3,
+        "Duration": 1000,
+        "Feedback": false
+    },
+    {
+        "Trial": 46,
+        "CorrRes": 3,
+        "Duration": 1000,
+        "Feedback": false
+    },
+    {
+        "Trial": 47,
+        "CorrRes": 2,
+        "Duration": 1000,
+        "Feedback": false
+    },
+    {
+        "Trial": 48,
+        "CorrRes": 3,
+        "Duration": 1000,
+        "Feedback": false
+    },
+    {
+        "Trial": 49,
+        "CorrRes": 1,
+        "Duration": 1000,
+        "Feedback": false
+    },
+    {
+        "Trial": 50,
+        "CorrRes": 2,
+        "Duration": 1000,
+        "Feedback": false
+    },
+    {
+        "Trial": 51,
+        "CorrRes": 2,
+        "Duration": 1000,
+        "Feedback": false
+    }
+];
---
+++ standalone/vOddball-Many/trials.js
@@ -0,0 +1,272 @@
+const trials = [
+    {
+        "Trial": 1,
+        "CorrRes": 3,
+        "Duration": 1500,
+        "Feedback": true
+    },
+    {
+        "Trial": 2,
+        "CorrRes": 1,
+        "Duration": 1500,
+        "Feedback": true
+    },
+    {
+        "Trial": 3,
+        "CorrRes": 2,
+        "Duration": 1500,
+        "Feedback": true
+    },
+    {
+        "Trial": 4,
+        "CorrRes": 2,
+        "Duration": 1500,
+        "Feedback": true
+    },
+    {
+        "Trial": 5,
+        "CorrRes": 3,
+        "Duration": 1500,
+        "Feedback": true
+    },
+    {
+        "Trial": 6,
+        "CorrRes": 2,
+        "Duration": 1500,
+        "Feedback": true
+    },
+    {
+        "Trial": 7,
+        "CorrRes": 3,
+        "Duration": 1500,
+        "Feedback": true
+    },
+    {
+        "Trial": 8,
+        "CorrRes": 2,
+        "Duration": 1500,
+        "Feedback": true
+    },
+    {
+        "Trial": 9,
+        "CorrRes": 2,
+        "Duration": 1500,
+        "Feedback": true
+    },
+    {
+        "Trial": 10,
+        "CorrRes": 1,
+        "Duration": 1500,
+        "Feedback": true
+    },
+    {
+        "Trial": 11,
+        "CorrRes": 1,
+        "Duration": 1500,
+        "Feedback": true
+    },
+    {
+        "Trial": 12,
+        "CorrRes": 2,
+        "Duration": 1500,
+        "Feedback": true
+    },
+    {
+        "Trial": 13,
+        "CorrRes": 3,
+        "Duration": 1000,
+        "Feedback": true
+    },
+    {
+        "Trial": 14,
+        "CorrRes": 1,
+        "Duration": 1000,
+        "Feedback": true
+    },
+    {
+        "Trial": 15,
+        "CorrRes": 2,
+        "Duration": 1000,
+        "Feedback": true
+    },
+    {
+        "Trial": 16,
+        "CorrRes": 3,
+        "Duration": 1000,
+        "Feedback": true
+    },
+    {
+        "Trial": 17,
+        "CorrRes": 1,
+        "Duration": 1000,
+        "Feedback": true
+    },
+    {
+        "Trial": 18,
+        "CorrRes": 2,
+        "Duration": 1000,
+        "Feedback": true
+    },
+    {
+        "Trial": 19,
+        "CorrRes": 1,
+        "Duration": 1000,
+        "Feedback": true
+    },
+    {
+        "Trial": 20,
+        "CorrRes": 3,
+        "Duration": 750,
+        "Feedback": true
+    },
+    {
+        "Trial": 21,
+        "CorrRes": 1,
+        "Duration": 750,
+        "Feedback": true
+    },
+    {
+        "Trial": 22,
+        "CorrRes": 3,
+        "Duration": 750,
+        "Feedback": true
+    },
+    {
+        "Trial": 23,
+        "CorrRes": 1,
+        "Duration": 750,
+        "Feedback": true
+    },
+    {
+        "Trial": 24,
+        "CorrRes": 1,
+        "Duration": 750,
+        "Feedback": true
+    },
+    {
+        "Trial": 25,
+        "CorrRes": 2,
+        "Duration": 750,
+        "Feedback": true
+    },
+    {
+        "Trial": 26,
+        "CorrRes": 2,
+        "Duration": 750,
+        "Feedback": true
+    },
+    {
+        "Trial": 27,
+        "CorrRes": 2,
+        "Duration": 750,
+        "Feedback": true
+    },
+    {
+        "Trial": 28,
+        "CorrRes": 1,
+        "Duration": 750,
+        "Feedback": true
+    },
+    {
+        "Trial": 29,
+        "CorrRes": 1,
+        "Duration": 750,
+        "Feedback": true
+    },
+    {
+        "Trial": 30,
+        "CorrRes": 2,
+        "Duration": 750,
+        "Feedback": true
+    },
+    {
+        "Trial": 31,
+        "CorrRes": 3,
+        "Duration": 750,
+        "Feedback": true
+    },
+    {
+        "Trial": 32,
+        "CorrRes": 1,
+        "Duration": 750,
+        "Feedback": true
+    },
+    {
+        "Trial": 33,
+        "CorrRes": 1,
+        "Duration": 750,
+        "Feedback": true
+    },
+    {
+        "Trial": 34,
+        "CorrRes": 2,
+        "Duration": 750,
+        "Feedback": true
+    },
+    {
+        "Trial": 35,
+        "CorrRes": 1,
+        "Duration": 750,
+        "Feedback": true
+    },
+    {
+        "Trial": 36,
+        "CorrRes": 3,
+        "Duration": 750,
+        "Feedback": true
+    },
+    {
+        "Trial": 37,
+        "CorrRes": 3,
+        "Duration": 1000,
+        "Feedback": true
+    },
+    {
+        "Trial": 38,
+        "CorrRes": 1,
+        "Duration": 1000,
+        "Feedback": true
+    },
+    {
+        "Trial": 39,
+        "CorrRes": 2,
+        "Duration": 1000,
+        "Feedback": true
+    },
+    {
+        "Trial": 40,
+        "CorrRes": 2,
+        "Duration": 1500,
+        "Feedback": true
+    },
+    {
+        "Trial": 41,
+        "CorrRes": 2,
+        "Duration": 1500,
+        "Feedback": true
+    },
+    {
+        "Trial": 42,
+        "CorrRes": 1,
+        "Duration": 3000,
+        "Feedback": true
+    },
+    {
+        "Trial": 43,
+        "CorrRes": 1,
+        "Duration": 3000,
+        "Feedback": true
+    },
+    {
+        "Trial": 44,
+        "CorrRes": 3,
+        "Duration": 3000,
+        "Feedback": true
+    },
+    {
+        "Trial": 45,
+        "CorrRes": 3,
+        "Duration": 4000,
+        "Feedback": true
+    }
+];
