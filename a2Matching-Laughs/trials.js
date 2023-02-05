9340f966f3264a8986ba27304dce5e99170575ca
JasonKChow <jason.k.chow@vanderbilt.edu>
2023-02-05 13:33:40 -0600

    new trials

--- a2Matching-Laughs/trials.js
+++ a2Matching-Laughs/trials.js
@@ -1,202 +1,218 @@
 const trials = [
     {
         "TrialN": 1,
-        "Target": "FS210D1.WAV",
-        "Choice1": "FS210D12.WAV",
-        "Choice2": "FS163D2.WAV",
-        "CorrRes": 1,
-        "DiffScore": 1145.8080948675
+        "Target": "FS213D1.WAV",
+        "Choice1": "FS131S8.WAV",
+        "Choice2": "FS213S11.WAV",
+        "CorrRes": 2,
+        "DiffScore": 6960.7214619088
     },
     {
         "TrialN": 2,
-        "Target": "FS213D4.WAV",
-        "Choice1": "FS213D1.WAV",
-        "Choice2": "FS100D4.WAV",
-        "CorrRes": 1,
-        "DiffScore": 1016.2858266884
+        "Target": "FS133D3.WAV",
+        "Choice1": "FS186S7.WAV",
+        "Choice2": "FS133S1.WAV",
+        "CorrRes": 2,
+        "DiffScore": 6410.1689327017
     },
     {
         "TrialN": 3,
-        "Target": "FS153D5.WAV",
-        "Choice1": "FS158S3.WAV",
-        "Choice2": "FS153S3.WAV",
-        "CorrRes": 2,
-        "DiffScore": 792.4515586655
+        "Target": "FS133D3.WAV",
+        "Choice1": "FS133S1.WAV",
+        "Choice2": "FS186S7.WAV",
+        "CorrRes": 1,
+        "DiffScore": 6410.1689327017
     },
     {
         "TrialN": 4,
-        "Target": "FS104D11.WAV",
-        "Choice1": "FS201S5.WAV",
-        "Choice2": "FS104D6.WAV",
+        "Target": "FS104D6.WAV",
+        "Choice1": "FS201S2.WAV",
+        "Choice2": "FS104D1.WAV",
         "CorrRes": 2,
-        "DiffScore": 408.0942950151
+        "DiffScore": 4584.8388161469
     },
     {
         "TrialN": 5,
-        "Target": "FS133D3.WAV",
-        "Choice1": "FS158S2.WAV",
-        "Choice2": "FS133S1.WAV",
-        "CorrRes": 2,
-        "DiffScore": 390.9068307476
+        "Target": "FS104D6.WAV",
+        "Choice1": "FS104D1.WAV",
+        "Choice2": "FS201S2.WAV",
+        "CorrRes": 1,
+        "DiffScore": 4584.8388161469
     },
     {
         "TrialN": 6,
-        "Target": "FS219S7.WAV",
-        "Choice1": "FS131S17.WAV",
-        "Choice2": "FS219D4.WAV",
-        "CorrRes": 2,
-        "DiffScore": 111.1089522287
+        "Target": "FS213S11.WAV",
+        "Choice1": "FS213D1.WAV",
+        "Choice2": "FS221D10.WAV",
+        "CorrRes": 1,
+        "DiffScore": 4177.1119298993
     },
     {
         "TrialN": 7,
-        "Target": "FS219S7.WAV",
-        "Choice1": "FS131S17.WAV",
-        "Choice2": "FS219D4.WAV",
-        "CorrRes": 2,
-        "DiffScore": 111.1089522287
+        "Target": "FS210D12.WAV",
+        "Choice1": "FS210S10.WAV",
+        "Choice2": "FS201S5.WAV",
+        "CorrRes": 1,
+        "DiffScore": 1937.1955457792
     },
     {
         "TrialN": 8,
-        "Target": "FS210D1.WAV",
-        "Choice1": "FS201S7.WAV",
-        "Choice2": "FS210D12.WAV",
-        "CorrRes": 2,
-        "DiffScore": 88.6585768897
+        "Target": "FS221D2.WAV",
+        "Choice1": "FS221D12.WAV",
+        "Choice2": "FS213D4.WAV",
+        "CorrRes": 1,
+        "DiffScore": 1677.500209236
     },
     {
         "TrialN": 9,
-        "Target": "FS104D11.WAV",
-        "Choice1": "FS210D14.WAV",
-        "Choice2": "FS104D6.WAV",
-        "CorrRes": 2,
-        "DiffScore": 34.0204920409
+        "Target": "FS210D4.WAV",
+        "Choice1": "FS210D12.WAV",
+        "Choice2": "FS174S7.WAV",
+        "CorrRes": 1,
+        "DiffScore": 1539.4574581602
     },
     {
         "TrialN": 10,
-        "Target": "FS186S5.WAV",
-        "Choice1": "FS104D11.WAV",
-        "Choice2": "FS186S7.WAV",
-        "CorrRes": 2,
-        "DiffScore": 12.2807268124
+        "Target": "FS210D1.WAV",
+        "Choice1": "FS210D4.WAV",
+        "Choice2": "FS163D2.WAV",
+        "CorrRes": 1,
+        "DiffScore": 1209.924667398
     },
     {
         "TrialN": 11,
-        "Target": "FS201S7.WAV",
-        "Choice1": "FS201S2.WAV",
-        "Choice2": "FS104D6.WAV",
+        "Target": "FS221D2.WAV",
+        "Choice1": "FS221S3.WAV",
+        "Choice2": "FS213D4.WAV",
         "CorrRes": 1,
-        "DiffScore": 12.0631236453
+        "DiffScore": 991.2376635061
     },
     {
         "TrialN": 12,
-        "Target": "FS142S6.WAV",
-        "Choice1": "FS142D3.WAV",
-        "Choice2": "FS218S2.WAV",
+        "Target": "FS131S17.WAV",
+        "Choice1": "FS131S16.WAV",
+        "Choice2": "FS153D5.WAV",
         "CorrRes": 1,
-        "DiffScore": 1.6466796326
+        "DiffScore": 830.9624845921
     },
     {
         "TrialN": 13,
-        "Target": "FS125D4.WAV",
-        "Choice1": "FS158S4.WAV",
-        "Choice2": "FS125D3.WAV",
+        "Target": "FS131S17.WAV",
+        "Choice1": "FS153D5.WAV",
+        "Choice2": "FS131S16.WAV",
         "CorrRes": 2,
-        "DiffScore": -17.4498995431
+        "DiffScore": 830.9624845921
     },
     {
         "TrialN": 14,
-        "Target": "FS125D4.WAV",
-        "Choice1": "FS125D3.WAV",
-        "Choice2": "FS158S4.WAV",
-        "CorrRes": 1,
-        "DiffScore": -17.4498995431
+        "Target": "FS158S3.WAV",
+        "Choice1": "FS125D4.WAV",
+        "Choice2": "FS158S2.WAV",
+        "CorrRes": 2,
+        "DiffScore": 601.7076757185
     },
     {
         "TrialN": 15,
-        "Target": "FS213D4.WAV",
-        "Choice1": "FS182S7.WAV",
-        "Choice2": "FS213D1.WAV",
+        "Target": "FS231S6.WAV",
+        "Choice1": "FS188S1.WAV",
+        "Choice2": "FS231D11.WAV",
         "CorrRes": 2,
-        "DiffScore": -20.4332036165
+        "DiffScore": 425.9706886255
     },
     {
         "TrialN": 16,
-        "Target": "FS131S17.WAV",
-        "Choice1": "FS131S16.WAV",
-        "Choice2": "FS213D4.WAV",
+        "Target": "FS158S2.WAV",
+        "Choice1": "FS158S4.WAV",
+        "Choice2": "FS218S2.WAV",
         "CorrRes": 1,
-        "DiffScore": -32.8422647669
+        "DiffScore": 407.8157310559
     },
     {
         "TrialN": 17,
-        "Target": "FS201S7.WAV",
-        "Choice1": "FS201S2.WAV",
-        "Choice2": "FS133S1.WAV",
-        "CorrRes": 1,
-        "DiffScore": -1952.7505667754
+        "Target": "FS210S10.WAV",
+        "Choice1": "FS174S7.WAV",
+        "Choice2": "FS210D1.WAV",
+        "CorrRes": 2,
+        "DiffScore": 245.1597024169
     },
     {
         "TrialN": 18,
-        "Target": "FS186S5.WAV",
-        "Choice1": "FS186S7.WAV",
-        "Choice2": "FS182S6.WAV",
+        "Target": "FS182S4.WAV",
+        "Choice1": "FS182S6.WAV",
+        "Choice2": "FS231D11.WAV",
         "CorrRes": 1,
-        "DiffScore": -3333.0487933187
+        "DiffScore": 183.4067309831
     },
     {
         "TrialN": 19,
-        "Target": "FS210D1.WAV",
-        "Choice1": "FS182S6.WAV",
-        "Choice2": "FS210D12.WAV",
-        "CorrRes": 2,
-        "DiffScore": -3434.5661143825
+        "Target": "FS201S5.WAV",
+        "Choice1": "FS201S7.WAV",
+        "Choice2": "FS104D11.WAV",
+        "CorrRes": 1,
+        "DiffScore": 176.8725394688
     },
     {
         "TrialN": 20,
-        "Target": "FS219S7.WAV",
-        "Choice1": "FS219D4.WAV",
-        "Choice2": "FS213S11.WAV",
-        "CorrRes": 1,
-        "DiffScore": -3531.5085993098
+        "Target": "FS142S6.WAV",
+        "Choice1": "FS232D4.WAV",
+        "Choice2": "FS142D3.WAV",
+        "CorrRes": 2,
+        "DiffScore": 95.5872640655
     },
     {
         "TrialN": 21,
-        "Target": "FS153D5.WAV",
-        "Choice1": "FS153S3.WAV",
-        "Choice2": "FS213S11.WAV",
-        "CorrRes": 1,
-        "DiffScore": -4192.4630964345
+        "Target": "FS142S6.WAV",
+        "Choice1": "FS232D4.WAV",
+        "Choice2": "FS142D3.WAV",
+        "CorrRes": 2,
+        "DiffScore": 95.5872640655
     },
     {
         "TrialN": 22,
-        "Target": "FS125D4.WAV",
-        "Choice1": "FS125D3.WAV",
-        "Choice2": "FS213S11.WAV",
-        "CorrRes": 1,
-        "DiffScore": -4582.928553222
+        "Target": "FS182S7.WAV",
+        "Choice1": "FS131S8.WAV",
+        "Choice2": "FS182S4.WAV",
+        "CorrRes": 2,
+        "DiffScore": 25.2526706034
     },
     {
         "TrialN": 23,
-        "Target": "FS158S3.WAV",
-        "Choice1": "FS104D1.WAV",
-        "Choice2": "FS158S4.WAV",
-        "CorrRes": 2,
-        "DiffScore": -4663.5651460303
+        "Target": "FS182S7.WAV",
+        "Choice1": "FS182S6.WAV",
+        "Choice2": "FS218S2.WAV",
+        "CorrRes": 1,
+        "DiffScore": -38.7029916406
     },
     {
         "TrialN": 24,
-        "Target": "FS142S6.WAV",
-        "Choice1": "FS142D3.WAV",
-        "Choice2": "FS104D1.WAV",
+        "Target": "FS158S3.WAV",
+        "Choice1": "FS158S4.WAV",
+        "Choice2": "FS125D4.WAV",
         "CorrRes": 1,
-        "DiffScore": -6298.4362311543
+        "DiffScore": -52.3859591418
     },
     {
         "TrialN": 25,
-        "Target": "FS182S7.WAV",
-        "Choice1": "FS104D1.WAV",
-        "Choice2": "FS182S6.WAV",
+        "Target": "FS221D12.WAV",
+        "Choice1": "FS163D2.WAV",
+        "Choice2": "FS221S3.WAV",
         "CorrRes": 2,
-        "DiffScore": -6763.7391073073
+        "DiffScore": -144.4236313332
+    },
+    {
+        "TrialN": 26,
+        "Target": "FS219S7.WAV",
+        "Choice1": "FS210D14.WAV",
+        "Choice2": "FS219D4.WAV",
+        "CorrRes": 2,
+        "DiffScore": -240.4289039626
+    },
+    {
+        "TrialN": 27,
+        "Target": "FS219D4.WAV",
+        "Choice1": "FS186S5.WAV",
+        "Choice2": "FS219S7.WAV",
+        "CorrRes": 2,
+        "DiffScore": -1145.48646972
     }
 ]
