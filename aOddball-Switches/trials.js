2bca8ca20bb1dc18f31646ddea83d7bd9bf48f38
JasonKChow <jason.k.chow@vanderbilt.edu>
2023-02-07 22:46:20 -0600

    change all trials to noise

--- aOddball-Switches/trials.js
+++ aOddball-Switches/trials.js
@@ -1,303 +1,303 @@
 let trials = [
     {
         "TrialN": 1,
-        "Stim1": "KailhSpeedBronze6.mp3",
-        "Stim2": "KailhSpeedBronze5.mp3",
-        "Stim3": "CherryRed5.mp3",
+        "Stim1": "KailhSpeedBronze6Noise.mp3",
+        "Stim2": "KailhSpeedBronze5Noise.mp3",
+        "Stim3": "CherryRed5Noise.mp3",
         "CorrRes": 3,
         "Pair": "KailhSpeedBronze",
         "Oddball": "CherryRed",
-        "Noise": "FALSE"
+
     },
     {
         "TrialN": 2,
-        "Stim1": "CherryRed5.mp3",
-        "Stim2": "CherryRed3.mp3",
-        "Stim3": "KailhSpeedBronze3.mp3",
+        "Stim1": "CherryRed5Noise.mp3",
+        "Stim2": "CherryRed3Noise.mp3",
+        "Stim3": "KailhSpeedBronze3Noise.mp3",
         "CorrRes": 3,
         "Pair": "CherryRed",
         "Oddball": "KailhSpeedBronze",
-        "Noise": "FALSE"
+
     },
     {
         "TrialN": 3,
-        "Stim1": "CherryRed1.mp3",
-        "Stim2": "GateronSilentBrown6.mp3",
-        "Stim3": "GateronSilentBrown2.mp3",
+        "Stim1": "CherryRed1Noise.mp3",
+        "Stim2": "GateronSilentBrown6Noise.mp3",
+        "Stim3": "GateronSilentBrown2Noise.mp3",
         "CorrRes": 1,
         "Pair": "GateronSilentBrown",
         "Oddball": "CherryRed",
-        "Noise": "FALSE"
+
     },
     {
         "TrialN": 4,
-        "Stim1": "CherryGreen6.mp3",
-        "Stim2": "CherryRed1.mp3",
-        "Stim3": "CherryRed2.mp3",
+        "Stim1": "CherryGreen6Noise.mp3",
+        "Stim2": "CherryRed1Noise.mp3",
+        "Stim3": "CherryRed2Noise.mp3",
         "CorrRes": 1,
         "Pair": "CherryRed",
         "Oddball": "CherryGreen",
-        "Noise": "FALSE"
+
     },
     {
         "TrialN": 5,
-        "Stim1": "Aliaz100g4.mp3",
-        "Stim2": "CherryGreen4.mp3",
-        "Stim3": "CherryGreen5.mp3",
+        "Stim1": "Aliaz100g4Noise.mp3",
+        "Stim2": "CherryGreen4Noise.mp3",
+        "Stim3": "CherryGreen5Noise.mp3",
         "CorrRes": 1,
         "Pair": "CherryGreen",
         "Oddball": "Aliaz100g",
-        "Noise": "FALSE"
+
     },
     {
         "TrialN": 6,
-        "Stim1": "CherryRed1.mp3",
-        "Stim2": "KailhSpeedBronze2.mp3",
-        "Stim3": "KailhSpeedBronze6.mp3",
+        "Stim1": "CherryRed1Noise.mp3",
+        "Stim2": "KailhSpeedBronze2Noise.mp3",
+        "Stim3": "KailhSpeedBronze6Noise.mp3",
         "CorrRes": 1,
         "Pair": "KailhSpeedBronze",
         "Oddball": "CherryRed",
-        "Noise": "FALSE"
+
     },
     {
         "TrialN": 7,
-        "Stim1": "CherryGreen6.mp3",
-        "Stim2": "CherryBrown6.mp3",
-        "Stim3": "CherryGreen4.mp3",
+        "Stim1": "CherryGreen6Noise.mp3",
+        "Stim2": "CherryBrown6Noise.mp3",
+        "Stim3": "CherryGreen4Noise.mp3",
         "CorrRes": 2,
         "Pair": "CherryGreen",
         "Oddball": "CherryBrown",
-        "Noise": "FALSE"
+
     },
     {
         "TrialN": 8,
-        "Stim1": "CherryRed5.mp3",
-        "Stim2": "CherryRed4.mp3",
-        "Stim3": "CherryGreen5.mp3",
+        "Stim1": "CherryRed5Noise.mp3",
+        "Stim2": "CherryRed4Noise.mp3",
+        "Stim3": "CherryGreen5Noise.mp3",
         "CorrRes": 3,
         "Pair": "CherryRed",
         "Oddball": "CherryGreen",
-        "Noise": "FALSE"
+
     },
     {
         "TrialN": 9,
-        "Stim1": "CherryGreen4.mp3",
-        "Stim2": "CherryBrown5.mp3",
-        "Stim3": "CherryBrown4.mp3",
+        "Stim1": "CherryGreen4Noise.mp3",
+        "Stim2": "CherryBrown5Noise.mp3",
+        "Stim3": "CherryBrown4Noise.mp3",
         "CorrRes": 1,
         "Pair": "CherryBrown",
         "Oddball": "CherryGreen",
-        "Noise": "FALSE"
+
     },
     {
         "TrialN": 10,
-        "Stim1": "CherryGreen1.mp3",
-        "Stim2": "Aliaz100g3.mp3",
-        "Stim3": "CherryGreen6.mp3",
+        "Stim1": "CherryGreen1Noise.mp3",
+        "Stim2": "Aliaz100g3Noise.mp3",
+        "Stim3": "CherryGreen6Noise.mp3",
         "CorrRes": 2,
         "Pair": "CherryGreen",
         "Oddball": "Aliaz100g",
-        "Noise": "FALSE"
+
     },
     {
         "TrialN": 11,
-        "Stim1": "KailhSpeedBronze3.mp3",
-        "Stim2": "KailhSpeedBronze6.mp3",
-        "Stim3": "GateronSilentBrown1.mp3",
+        "Stim1": "KailhSpeedBronze3Noise.mp3",
+        "Stim2": "KailhSpeedBronze6Noise.mp3",
+        "Stim3": "GateronSilentBrown1Noise.mp3",
         "CorrRes": 3,
         "Pair": "KailhSpeedBronze",
         "Oddball": "GateronSilentBrown",
-        "Noise": "FALSE"
+
     },
     {
         "TrialN": 12,
-        "Stim1": "CherryRed1.mp3",
-        "Stim2": "CherryGreen3.mp3",
-        "Stim3": "CherryGreen5.mp3",
+        "Stim1": "CherryRed1Noise.mp3",
+        "Stim2": "CherryGreen3Noise.mp3",
+        "Stim3": "CherryGreen5Noise.mp3",
         "CorrRes": 1,
         "Pair": "CherryGreen",
         "Oddball": "CherryRed",
-        "Noise": "FALSE"
+
     },
     {
         "TrialN": 13,
-        "Stim1": "GateronSilentBrown1.mp3",
-        "Stim2": "CherryGreen3.mp3",
-        "Stim3": "GateronSilentBrown5.mp3",
+        "Stim1": "GateronSilentBrown1Noise.mp3",
+        "Stim2": "CherryGreen3Noise.mp3",
+        "Stim3": "GateronSilentBrown5Noise.mp3",
         "CorrRes": 2,
         "Pair": "GateronSilentBrown",
         "Oddball": "CherryGreen",
-        "Noise": "FALSE"
+
     },
     {
         "TrialN": 14,
-        "Stim1": "KailhSpeedBronze5.mp3",
-        "Stim2": "GateronSilentBrown6.mp3",
-        "Stim3": "GateronSilentBrown5.mp3",
+        "Stim1": "KailhSpeedBronze5Noise.mp3",
+        "Stim2": "GateronSilentBrown6Noise.mp3",
+        "Stim3": "GateronSilentBrown5Noise.mp3",
         "CorrRes": 1,
         "Pair": "GateronSilentBrown",
         "Oddball": "KailhSpeedBronze",
-        "Noise": "FALSE"
+
     },
     {
         "TrialN": 15,
-        "Stim1": "CherryBrown5.mp3",
-        "Stim2": "KailhSpeedBronze4.mp3",
-        "Stim3": "KailhSpeedBronze5.mp3",
+        "Stim1": "CherryBrown5Noise.mp3",
+        "Stim2": "KailhSpeedBronze4Noise.mp3",
+        "Stim3": "KailhSpeedBronze5Noise.mp3",
         "CorrRes": 1,
         "Pair": "KailhSpeedBronze",
         "Oddball": "CherryBrown",
-        "Noise": "FALSE"
+
     },
     {
         "TrialN": 16,
-        "Stim1": "CherryRed5.mp3",
-        "Stim2": "CherryRed6.mp3",
-        "Stim3": "KailhSpeedBronze4.mp3",
+        "Stim1": "CherryRed5Noise.mp3",
+        "Stim2": "CherryRed6Noise.mp3",
+        "Stim3": "KailhSpeedBronze4Noise.mp3",
         "CorrRes": 3,
         "Pair": "CherryRed",
         "Oddball": "KailhSpeedBronze",
-        "Noise": "FALSE"
+
     },
     {
         "TrialN": 17,
-        "Stim1": "CherryRed4.mp3",
-        "Stim2": "CherryRed6.mp3",
-        "Stim3": "Aliaz100g4.mp3",
+        "Stim1": "CherryRed4Noise.mp3",
+        "Stim2": "CherryRed6Noise.mp3",
+        "Stim3": "Aliaz100g4Noise.mp3",
         "CorrRes": 2,
         "Pair": "CherryRed",
         "Oddball": "Aliaz100g",
-        "Noise": "FALSE"
+
     },
     {
         "TrialN": 18,
-        "Stim1": "KailhSpeedBronze6.mp3",
-        "Stim2": "Aliaz100g1.mp3",
-        "Stim3": "Aliaz100g6.mp3",
+        "Stim1": "KailhSpeedBronze6Noise.mp3",
+        "Stim2": "Aliaz100g1Noise.mp3",
+        "Stim3": "Aliaz100g6Noise.mp3",
         "CorrRes": 1,
         "Pair": "Aliaz100g",
         "Oddball": "KailhSpeedBronze",
-        "Noise": "FALSE"
+
     },
     {
         "TrialN": 19,
-        "Stim1": "Aliaz100g2.mp3",
-        "Stim2": "Aliaz100g3.mp3",
-        "Stim3": "GateronSilentBrown6.mp3",
+        "Stim1": "Aliaz100g2Noise.mp3",
+        "Stim2": "Aliaz100g3Noise.mp3",
+        "Stim3": "GateronSilentBrown6Noise.mp3",
         "CorrRes": 3,
         "Pair": "Aliaz100g",
         "Oddball": "GateronSilentBrown",
-        "Noise": "FALSE"
+
     },
     {
         "TrialN": 20,
-        "Stim1": "GateronSilentBrown4.mp3",
-        "Stim2": "CherryBrown4.mp3",
-        "Stim3": "GateronSilentBrown5.mp3",
+        "Stim1": "GateronSilentBrown4Noise.mp3",
+        "Stim2": "CherryBrown4Noise.mp3",
+        "Stim3": "GateronSilentBrown5Noise.mp3",
         "CorrRes": 2,
         "Pair": "GateronSilentBrown",
         "Oddball": "CherryBrown",
-        "Noise": "FALSE"
+
     },
     {
         "TrialN": 21,
-        "Stim1": "Aliaz100g6.mp3",
-        "Stim2": "CherryBrown2.mp3",
-        "Stim3": "CherryBrown3.mp3",
+        "Stim1": "Aliaz100g6Noise.mp3",
+        "Stim2": "CherryBrown2Noise.mp3",
+        "Stim3": "CherryBrown3Noise.mp3",
         "CorrRes": 1,
         "Pair": "CherryBrown",
         "Oddball": "Aliaz100g",
-        "Noise": "FALSE"
+
     },
     {
         "TrialN": 22,
-        "Stim1": "Aliaz100g6.mp3",
-        "Stim2": "CherryRed2.mp3",
-        "Stim3": "Aliaz100g2.mp3",
+        "Stim1": "Aliaz100g6Noise.mp3",
+        "Stim2": "CherryRed2Noise.mp3",
+        "Stim3": "Aliaz100g2Noise.mp3",
         "CorrRes": 2,
         "Pair": "Aliaz100g",
         "Oddball": "CherryRed",
-        "Noise": "FALSE"
+
     },
     {
         "TrialN": 23,
-        "Stim1": "GateronSilentBrown5.mp3",
-        "Stim2": "Aliaz100g4.mp3",
-        "Stim3": "Aliaz100g6.mp3",
+        "Stim1": "GateronSilentBrown5Noise.mp3",
+        "Stim2": "Aliaz100g4Noise.mp3",
+        "Stim3": "Aliaz100g6Noise.mp3",
         "CorrRes": 1,
         "Pair": "Aliaz100g",
         "Oddball": "GateronSilentBrown",
-        "Noise": "FALSE"
+
     },
     {
         "TrialN": 24,
-        "Stim1": "Aliaz100g4.mp3",
-        "Stim2": "GateronSilentBrown2.mp3",
-        "Stim3": "GateronSilentBrown1.mp3",
+        "Stim1": "Aliaz100g4Noise.mp3",
+        "Stim2": "GateronSilentBrown2Noise.mp3",
+        "Stim3": "GateronSilentBrown1Noise.mp3",
         "CorrRes": 1,
         "Pair": "GateronSilentBrown",
         "Oddball": "Aliaz100g",
-        "Noise": "FALSE"
+
     },
     {
         "TrialN": 25,
-        "Stim1": "KailhSpeedBronze3.mp3",
-        "Stim2": "CherryGreen3.mp3",
-        "Stim3": "CherryGreen4.mp3",
+        "Stim1": "KailhSpeedBronze3Noise.mp3",
+        "Stim2": "CherryGreen3Noise.mp3",
+        "Stim3": "CherryGreen4Noise.mp3",
         "CorrRes": 1,
         "Pair": "CherryGreen",
         "Oddball": "KailhSpeedBronze",
-        "Noise": "FALSE"
+
     },
     {
         "TrialN": 26,
-        "Stim1": "CherryBrown1.mp3",
-        "Stim2": "CherryBrown2.mp3",
-        "Stim3": "GateronSilentBrown6.mp3",
+        "Stim1": "CherryBrown1Noise.mp3",
+        "Stim2": "CherryBrown2Noise.mp3",
+        "Stim3": "GateronSilentBrown6Noise.mp3",
         "CorrRes": 3,
         "Pair": "CherryBrown",
         "Oddball": "GateronSilentBrown",
-        "Noise": "FALSE"
+
     },
     {
         "TrialN": 27,
-        "Stim1": "KailhSpeedBronze5.mp3",
-        "Stim2": "KailhSpeedBronze6.mp3",
-        "Stim3": "CherryGreen4.mp3",
+        "Stim1": "KailhSpeedBronze5Noise.mp3",
+        "Stim2": "KailhSpeedBronze6Noise.mp3",
+        "Stim3": "CherryGreen4Noise.mp3",
         "CorrRes": 3,
         "Pair": "KailhSpeedBronze",
         "Oddball": "CherryGreen",
-        "Noise": "FALSE"
+
     },
     {
         "TrialN": 28,
-        "Stim1": "CherryBrown3.mp3",
-        "Stim2": "CherryBrown4.mp3",
-        "Stim3": "CherryRed1.mp3",
+        "Stim1": "CherryBrown3Noise.mp3",
+        "Stim2": "CherryBrown4Noise.mp3",
+        "Stim3": "CherryRed1Noise.mp3",
         "CorrRes": 3,
         "Pair": "CherryBrown",
         "Oddball": "CherryRed",
-        "Noise": "FALSE"
+
     },
     {
         "TrialN": 29,
-        "Stim1": "CherryBrown6.mp3",
-        "Stim2": "CherryBrown5.mp3",
-        "Stim3": "CherryRed4.mp3",
+        "Stim1": "CherryBrown6Noise.mp3",
+        "Stim2": "CherryBrown5Noise.mp3",
+        "Stim3": "CherryRed4Noise.mp3",
         "CorrRes": 3,
         "Pair": "CherryBrown",
         "Oddball": "CherryRed",
-        "Noise": "FALSE"
+
     },
     {
         "TrialN": 30,
-        "Stim1": "GateronSilentBrown6.mp3",
-        "Stim2": "GateronSilentBrown5.mp3",
-        "Stim3": "Aliaz100g4.mp3",
+        "Stim1": "GateronSilentBrown6Noise.mp3",
+        "Stim2": "GateronSilentBrown5Noise.mp3",
+        "Stim3": "Aliaz100g4Noise.mp3",
         "CorrRes": 3,
         "Pair": "GateronSilentBrown",
         "Oddball": "Aliaz100g",
-        "Noise": "FALSE"
+
     },
     {
         "TrialN": 31,
@@ -307,7 +307,7 @@ let trials = [
         "CorrRes": 1,
         "Pair": "Aliaz100g",
         "Oddball": "CherryGreen",
-        "Noise": "TRUE"
+
     },
     {
         "TrialN": 32,
@@ -317,7 +317,7 @@ let trials = [
         "CorrRes": 1,
         "Pair": "GateronSilentBrown",
         "Oddball": "CherryGreen",
-        "Noise": "TRUE"
+
     },
     {
         "TrialN": 33,
@@ -327,7 +327,7 @@ let trials = [
         "CorrRes": 1,
         "Pair": "CherryGreen",
         "Oddball": "GateronSilentBrown",
-        "Noise": "TRUE"
+
     },
     {
         "TrialN": 34,
@@ -337,7 +337,7 @@ let trials = [
         "CorrRes": 1,
         "Pair": "CherryBrown",
         "Oddball": "CherryGreen",
-        "Noise": "TRUE"
+
     },
     {
         "TrialN": 35,
@@ -347,7 +347,7 @@ let trials = [
         "CorrRes": 2,
         "Pair": "KailhSpeedBronze",
         "Oddball": "Aliaz100g",
-        "Noise": "TRUE"
+
     },
     {
         "TrialN": 36,
@@ -357,7 +357,7 @@ let trials = [
         "CorrRes": 2,
         "Pair": "CherryGreen",
         "Oddball": "CherryBrown",
-        "Noise": "TRUE"
+
     },
     {
         "TrialN": 37,
@@ -367,7 +367,7 @@ let trials = [
         "CorrRes": 3,
         "Pair": "GateronSilentBrown",
         "Oddball": "KailhSpeedBronze",
-        "Noise": "TRUE"
+
     },
     {
         "TrialN": 38,
@@ -377,7 +377,7 @@ let trials = [
         "CorrRes": 1,
         "Pair": "GateronSilentBrown",
         "Oddball": "CherryRed",
-        "Noise": "TRUE"
+
     },
     {
         "TrialN": 39,
@@ -387,7 +387,7 @@ let trials = [
         "CorrRes": 3,
         "Pair": "CherryGreen",
         "Oddball": "CherryRed",
-        "Noise": "TRUE"
+
     },
     {
         "TrialN": 40,
@@ -397,7 +397,7 @@ let trials = [
         "CorrRes": 2,
         "Pair": "Aliaz100g",
         "Oddball": "CherryGreen",
-        "Noise": "TRUE"
+
     },
     {
         "TrialN": 41,
@@ -407,7 +407,7 @@ let trials = [
         "CorrRes": 2,
         "Pair": "CherryRed",
         "Oddball": "GateronSilentBrown",
-        "Noise": "TRUE"
+
     },
     {
         "TrialN": 42,
@@ -417,7 +417,7 @@ let trials = [
         "CorrRes": 3,
         "Pair": "CherryRed",
         "Oddball": "GateronSilentBrown",
-        "Noise": "TRUE"
+
     },
     {
         "TrialN": 43,
@@ -427,7 +427,7 @@ let trials = [
         "CorrRes": 2,
         "Pair": "KailhSpeedBronze",
         "Oddball": "CherryBrown",
-        "Noise": "TRUE"
+
     },
     {
         "TrialN": 44,
@@ -437,7 +437,7 @@ let trials = [
         "CorrRes": 2,
         "Pair": "CherryBrown",
         "Oddball": "KailhSpeedBronze",
-        "Noise": "TRUE"
+
     },
     {
         "TrialN": 45,
@@ -447,7 +447,7 @@ let trials = [
         "CorrRes": 3,
         "Pair": "CherryGreen",
         "Oddball": "GateronSilentBrown",
-        "Noise": "TRUE"
+
     },
     {
         "TrialN": 46,
@@ -457,7 +457,7 @@ let trials = [
         "CorrRes": 2,
         "Pair": "CherryBrown",
         "Oddball": "KailhSpeedBronze",
-        "Noise": "TRUE"
+
     },
     {
         "TrialN": 47,
@@ -467,7 +467,7 @@ let trials = [
         "CorrRes": 2,
         "Pair": "KailhSpeedBronze",
         "Oddball": "Aliaz100g",
-        "Noise": "TRUE"
+
     },
     {
         "TrialN": 48,
@@ -477,7 +477,7 @@ let trials = [
         "CorrRes": 2,
         "Pair": "KailhSpeedBronze",
         "Oddball": "GateronSilentBrown",
-        "Noise": "TRUE"
+
     },
     {
         "TrialN": 49,
@@ -487,7 +487,7 @@ let trials = [
         "CorrRes": 3,
         "Pair": "Aliaz100g",
         "Oddball": "KailhSpeedBronze",
-        "Noise": "TRUE"
+
     },
     {
         "TrialN": 50,
@@ -497,7 +497,7 @@ let trials = [
         "CorrRes": 3,
         "Pair": "CherryRed",
         "Oddball": "Aliaz100g",
-        "Noise": "TRUE"
+
     },
     {
         "TrialN": 51,
@@ -507,7 +507,7 @@ let trials = [
         "CorrRes": 2,
         "Pair": "CherryBrown",
         "Oddball": "Aliaz100g",
-        "Noise": "TRUE"
+
     },
     {
         "TrialN": 52,
@@ -517,7 +517,7 @@ let trials = [
         "CorrRes": 2,
         "Pair": "Aliaz100g",
         "Oddball": "CherryBrown",
-        "Noise": "TRUE"
+
     },
     {
         "TrialN": 53,
@@ -527,7 +527,7 @@ let trials = [
         "CorrRes": 3,
         "Pair": "Aliaz100g",
         "Oddball": "CherryRed",
-        "Noise": "TRUE"
+
     },
     {
         "TrialN": 54,
@@ -537,7 +537,7 @@ let trials = [
         "CorrRes": 2,
         "Pair": "GateronSilentBrown",
         "Oddball": "CherryBrown",
-        "Noise": "TRUE"
+
     },
     {
         "TrialN": 55,
@@ -547,7 +547,7 @@ let trials = [
         "CorrRes": 3,
         "Pair": "Aliaz100g",
         "Oddball": "CherryBrown",
-        "Noise": "TRUE"
+
     },
     {
         "TrialN": 56,
@@ -557,7 +557,7 @@ let trials = [
         "CorrRes": 1,
         "Pair": "CherryBrown",
         "Oddball": "GateronSilentBrown",
-        "Noise": "TRUE"
+
     },
     {
         "TrialN": 57,
@@ -567,7 +567,7 @@ let trials = [
         "CorrRes": 1,
         "Pair": "CherryRed",
         "Oddball": "CherryBrown",
-        "Noise": "TRUE"
+
     },
     {
         "TrialN": 58,
@@ -577,7 +577,7 @@ let trials = [
         "CorrRes": 1,
         "Pair": "CherryRed",
         "Oddball": "CherryBrown",
-        "Noise": "TRUE"
+
     },
     {
         "TrialN": 59,
@@ -587,7 +587,7 @@ let trials = [
         "CorrRes": 2,
         "Pair": "KailhSpeedBronze",
         "Oddball": "CherryGreen",
-        "Noise": "TRUE"
+
     },
     {
         "TrialN": 60,
@@ -597,6 +597,6 @@ let trials = [
         "CorrRes": 2,
         "Pair": "CherryGreen",
         "Oddball": "KailhSpeedBronze",
-        "Noise": "TRUE"
+
     }
 ];
